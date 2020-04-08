b945ae5c49f85e63a183dc614e8650048bc2c4e6
YOONGOO <dbsrlehs92@gmail.com>
2020-04-08 22:35:42 +0900

    Update naver-smartstore-site.js
    사이트 접속 제한 우회

--- kf-mask-watcher/site/naver-smartstore-site.js
+++ kf-mask-watcher/site/naver-smartstore-site.js
@@ -11,20 +11,26 @@ class NaverSmartstoreSite extends BaseSite {
   }
   
   async browse() {
-    const $ = await request({
-      url: this._url,
-      transform: (body) => cheerio.load(body),
-      encoding: null
-    });
+    try {
+      const $ = await request({
+        url: this._url,
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15",
+        },
+        transform: (body) => cheerio.load(body)
+      });
 
-    const clues = [
-      !$(".buy .mask2").hasClass("mask2"),
-      $(".buy ._productPreLaunch").hasClass("_productPreLaunch")
-    ];
-    
-    this._hasProduct = _.includes(_.map(clues, clue => clue), true);
+      const clues = [
+        !$(".buy .mask2").hasClass("mask2"),
+        $(".buy ._productPreLaunch").hasClass("_productPreLaunch")
+      ];
+      
+      this._hasProduct = _.includes(_.map(clues, clue => clue), true);
 
-    console.log(`Succeed to browse site. name=${this._name}\tcan_buy=${this._hasProduct}\turl=${this._url} `);
+      console.log(`Succeed to browse site. name=${this._name}\tcan_buy=${this._hasProduct}\turl=${this._url} `);
+    } catch(err) {
+      console.log(`${err}`);
+    }
   }
 }
 
