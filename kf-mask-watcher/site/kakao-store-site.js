bd010f216da76a8170ddcec0376347578a19c06e
YOONGOO <dbsrlehs92@gmail.com>
2020-04-08 23:16:02 +0900

    카카오 스토어 추가

--- kf-mask-watcher/site/kakao-store-site.js
+++ kf-mask-watcher/site/kakao-store-site.js
@@ -1,12 +1,11 @@
 "use strict";
-
 const cheerio = require("cheerio");
 const request = require("request-promise");
 const _ = require("lodash");
 const config = require("config");
 const BaseSite = require("./base-site");
 
-class NaverSmartstoreSite extends BaseSite {
+class KakaoStoreSite extends BaseSite {
   constructor(id, name, url) {
     super(id, name, url);
   }
@@ -22,10 +21,10 @@ class NaverSmartstoreSite extends BaseSite {
       });
 
       const clues = [
-        !$(".buy .mask2").hasClass("mask2"),
-        $(".buy ._productPreLaunch").hasClass("_productPreLaunch")
+        !_.isEqual($("._purchase_button").attr("disabled"), "disabled")
       ];
       
+      console.log(clues[0]);
       this._hasProduct = _.includes(_.map(clues, clue => clue), true);
 
       console.log(`Succeed to browse site. name=${this._name}\tcan_buy=${this._hasProduct}\turl=${this._url} `);
@@ -35,4 +34,4 @@ class NaverSmartstoreSite extends BaseSite {
   }
 }
 
-module.exports = NaverSmartstoreSite;
+module.exports = KakaoStoreSite;
